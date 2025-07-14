a0ca82a48e0eaccc30c72da634fab51b0c346d97
MiheerDGH <miheerdhar@gmail.com>
2025-07-14 19:22:01 -0400

    complete initial infrastructure setup with Supabase, Prisma, and migrations

--- src/controllers/docController.js
+++ src/controllers/docController.js
@@ -1,5 +1,5 @@
 const { supabase } = require('../config/supabaseClient');
-const { PrimsaClient } = require('@prisma/client'); // adjust if needed
+const { PrismaClient } = require('@prisma/client'); // adjust if needed
 const prisma = new PrismaClient;
 const { v4: uuidv4 } = require('uuid');
 
